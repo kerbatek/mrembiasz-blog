7ffb809236c9489838a13b92224febf9f0540399
kerbatek <mateusz@mrembiasz.pl>
2026-08-12 11:20:20 +0200

    chore: disable inline stylesheets

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config";
 
 export default defineConfig({
   build: {
-    inlineStylesheets: "always",
+    inlineStylesheets: "never",
   },
   output: "static",
 });
