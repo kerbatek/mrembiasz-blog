d55c8522e104e4ed3ac04020eebfb74f4bcd4537
Mateusz Rembiasz <mateusz@mrembiasz.pl>
2026-08-14 14:49:23 +0200

    feat: cicd status improvements, implement cloudfront logging (#10)
    * feat: add cloudfront logging to s3 bucket
    
    * feat: remove binary tfplan from artifacts
    
    * refactor: clean up cicd jobs titles
    
    * fix: dont display redundant titles
    
    * refactor: github copilot cr

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -3,6 +3,11 @@ resource "aws_s3_bucket" "site" {
   tags   = local.tags
 }
 
+resource "aws_s3_bucket" "cloudfront_logs" {
+  bucket = "mrembiasz-blog-cloudfront-logs"
+  tags   = local.tags
+}
+
 resource "aws_s3_bucket_public_access_block" "site" {
   bucket = aws_s3_bucket.site.id
 
@@ -22,11 +27,89 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "site" {
   }
 }
 
+resource "aws_s3_bucket_public_access_block" "cloudfront_logs" {
+  bucket = aws_s3_bucket.cloudfront_logs.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_ownership_controls" "cloudfront_logs" {
+  bucket = aws_s3_bucket.cloudfront_logs.id
+
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+data "aws_canonical_user_id" "current" {}
+data "aws_cloudfront_log_delivery_canonical_user_id" "cloudfront" {}
+
+resource "aws_s3_bucket_acl" "cloudfront_logs" {
+  bucket = aws_s3_bucket.cloudfront_logs.id
+
+  access_control_policy {
+    owner {
+      id = data.aws_canonical_user_id.current.id
+    }
+
+    grant {
+      permission = "FULL_CONTROL"
+
+      grantee {
+        id   = data.aws_canonical_user_id.current.id
+        type = "CanonicalUser"
+      }
+    }
+
+    grant {
+      permission = "FULL_CONTROL"
+
+      grantee {
+        id   = data.aws_cloudfront_log_delivery_canonical_user_id.cloudfront.id
+        type = "CanonicalUser"
+      }
+    }
+  }
+
+  depends_on = [
+    aws_s3_bucket_ownership_controls.cloudfront_logs,
+    aws_s3_bucket_public_access_block.cloudfront_logs,
+  ]
+}
+
 resource "aws_s3_bucket_versioning" "site" {
   bucket = aws_s3_bucket.site.id
 
   versioning_configuration {
     status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "cloudfront_logs" {
+  bucket = aws_s3_bucket.cloudfront_logs.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "cloudfront_logs" {
+  bucket = aws_s3_bucket.cloudfront_logs.id
+
+  rule {
+    id     = "expire-cloudfront-logs"
+    status = "Enabled"
+
+    filter {}
+
+    expiration {
+      days = 90
+    }
   }
 }
 
