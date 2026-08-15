ae5db6d9d923fd790ea42034369e949944b39e95
kerbatek <mateusz@mrembiasz.pl>
2026-08-15 13:04:30 +0200

    fix: fix build lambda command

--- deploy/scripts/build-backend-lambdas.sh
+++ deploy/scripts/build-backend-lambdas.sh
@@ -2,4 +2,4 @@
 set -eu
 
 cd src/backend/analytics_validator
-GOOS=linux GOARCH=arm64 CGO_ENABLED=0 go build -trimpath -ldflags="-s -w" -o bootstrap .
+GOOS=linux GOARCH=arm64 CGO_ENABLED=0 go build -buildvcs=false -trimpath -ldflags="-s -w" -o bootstrap .
