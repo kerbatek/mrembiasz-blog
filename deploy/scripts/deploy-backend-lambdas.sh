a71dc66dee6f93fa772ebccadef633d923a41ac1
kerbatek <mateusz@mrembiasz.pl>
2026-08-15 16:37:44 +0200

    feat: dry terraform files

--- deploy/scripts/deploy-backend-lambdas.sh
+++ deploy/scripts/deploy-backend-lambdas.sh
@@ -1,12 +1,6 @@
 #!/usr/bin/env bash
 set -eu
 
-lambdas=(
-  "mrembiasz-blog-aggregate-views=deploy/backend-lambdas/aggregate_views.zip"
-  "mrembiasz-blog-analytics-validator=deploy/backend-lambdas/analytics_validator.zip"
-  "mrembiasz-blog-get-views=deploy/backend-lambdas/get_views.zip"
-)
-
 update_lambda() {
   function_name="$1"
   package_path="$2"
@@ -22,13 +16,16 @@ wait_lambda() {
   aws lambda wait function-updated --function-name "$function_name"
 }
 
-for lambda in "${lambdas[@]}"; do
-  function_name="${lambda%%=*}"
-  package_path="${lambda#*=}"
-  update_lambda "$function_name" "$package_path"
+lambda_dirs=(src/backend/lambdas/*)
+
+for lambda_dir in "${lambda_dirs[@]}"; do
+  lambda_name="$(basename "$lambda_dir")"
+  function_name="mrembiasz-blog-${lambda_name//_/-}"
+  update_lambda "$function_name" "deploy/backend-lambdas/${lambda_name}.zip"
 done
 
-for lambda in "${lambdas[@]}"; do
-  function_name="${lambda%%=*}"
+for lambda_dir in "${lambda_dirs[@]}"; do
+  lambda_name="$(basename "$lambda_dir")"
+  function_name="mrembiasz-blog-${lambda_name//_/-}"
   wait_lambda "$function_name"
 done
